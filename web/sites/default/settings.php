bb9da112d9312fcc518fe20fed066d6360de025e
Jeremy DeLong <jdelong@backofficethinking.com>
2019-11-12 13:25:00 -0500

    build #1

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -17,11 +17,62 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 include __DIR__ . "/settings.pantheon.php";
 
 /**
+ * Copying from settings.pantheon.php but adding for local.dev
+ */
+if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+  $settings['hash_salt'] = $_ENV['DRUPAL_HASH_SALT'];
+} else {
+  $settings['hash_salt'] = 'Xxm9F4VpJRMYgystF8qHyOxcoHU5Of59lI82PqkZQ5o=';
+}
+
+/**
  * Place the config directory outside of the Drupal root.
  */
 $config_directories = array(
   CONFIG_SYNC_DIRECTORY => dirname(DRUPAL_ROOT) . '/config',
 );
+
+$conf['https'] = TRUE;
+
+if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
+  // Redirect to https://$primary_domain in the Live environment
+  if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
+    // Replace www.example.com with your registered domain name.
+    $primary_domain = 'www.example.com';
+  }
+  else {
+    // Redirect to HTTPS on every Pantheon environment.
+    $primary_domain = $_SERVER['HTTP_HOST'];
+  }
+
+  if ($_SERVER['HTTP_HOST'] != $primary_domain
+      || !isset($_SERVER['HTTP_USER_AGENT_HTTPS'])
+      || $_SERVER['HTTP_USER_AGENT_HTTPS'] != 'ON' ) {
+
+    // Name transaction "redirect" in New Relic for improved reporting (optional).
+    if (extension_loaded('newrelic')) {
+      newrelic_name_transaction("redirect");
+    }
+
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: https://'. $primary_domain . $_SERVER['REQUEST_URI']);
+    exit();
+  }
+  // Drupal 8 Trusted Host Settings
+  if (is_array($settings)) {
+    $settings['trusted_host_patterns'] = array('^'. preg_quote($primary_domain) .'$');
+  }
+}
+
+
+/**
+ * If there is an environment settings file, then include it
+ */
+$envirosettings = __DIR__ . "/enviros/settings." . $_ENV['PANTHEON_ENVIRONMENT'] . ".php";
+if (file_exists($envirosettings)) {
+  include $envirosettings;
+}
+
 
 /**
  * If there is a local settings file, then include it
